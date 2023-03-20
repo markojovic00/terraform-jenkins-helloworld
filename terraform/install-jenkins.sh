9c8aa6eccbf3c716eaae4dff5b84f5860b8c55fa
jovicpt <markojovicpt@gmail.com>
2023-03-20 02:11:51 +0100

    add jenkins ownership and correction in installation

--- terraform/install-jenkins.sh
+++ terraform/install-jenkins.sh
@@ -7,28 +7,28 @@ sudo mkswap /swapfile
 sudo swapon /swapfile
 sudo cp /etc/fstab /etc/fstab.bak
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-sleep 30
-sudo apt update 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -o "Dpkg::Options::=--force-confold" dist-upgrade -y \
---allow-downgrades --allow-remove-essential --allow-change-held-packages
-# jenkins install
-sudo apt update 
-sudo apt install default-jre -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] \
-http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-# docker install
 sleep 10
+# update and upgrade
+sudo apt update 
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
+# jenkins install
+sudo apt install -y openjdk-11-jdk
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo "deb https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt update
+sudo apt-get install -y --no-install-recommends jenkins
+sudo sed -i '/\[Service\]/a TimeoutStartSec=600\nTimeoutStopUSec=infinity' /lib/systemd/system/jenkins.service
+sudo systemctl daemon-reload
+sudo systemctl restart jenkins
+sudo systemctl enable jenkins  
+# docker install
+sleep 20
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
 https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker $USER
 sudo usermod -aG docker jenkins
@@ -39,5 +39,10 @@ sudo apt-get install -y jq
 sudo curl -L --silent --location http://localhost:8080/jnlpJars/jenkins-cli.jar > jenkins-cli.jar
 java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:${JENKINS_ADMIN_PASSWORD} install-plugin \
 docker-plugin:1.3.0 docker-commons docker-workflow
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+# change ownership of Jenkins directory
+sudo systemctl stop jenkins
+sudo chown -R jenkins:jenkins /var/lib/jenkins
+sudo systemctl start jenkins
+# display jenkins admin password
+echo "Jenkins admin password: ${JENKINS_ADMIN_PASSWORD}"
 sudo sudo shutdown -r 1
